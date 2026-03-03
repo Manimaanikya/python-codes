735e2167a98922f1cab4636f72d04b948b89570e
Manikumar H K <manikumarmanuhk@gmail.com>
2026-03-03 22:42:27 +0530

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,13 @@
+list=['pooja','sowmya','maggie','mani']
+print(list)
+list.append('maanikya')
+print(list)
+
+print(list[3])
+
+print(list.reverse)
+rev=list.reverse()
+print(list)
+list.sort()
+print(list)
+print(rev)
