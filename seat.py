118fbd974e6ec39572f5340907baea821fb4812d
Manikumar H K <manikumarmanuhk@gmail.com>
2026-03-15 22:16:56 +0530

    Add files via upload

---
+++ seat.py
@@ -0,0 +1,7 @@
+seat=8
+while seat>0:
+    print('seat available')
+    seat=seat-1
+    print('remaining seat',seat)
+
+print('all seat are booked')
---
+++ time_table.py
@@ -0,0 +1,9 @@
+time=int(input('enter time : '))
+if time>7 and time<10:
+    print('this is tiffen time')
+elif time>12 and time<14:
+    print('this is lunch time')
+elif time>20 and time<22:
+    print('this is dinner time')
+else:
+    print('this is not meal time')
